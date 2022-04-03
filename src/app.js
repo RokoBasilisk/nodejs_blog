bf32e5337c684354411bfddcb1bd666d60b1bcfb
RokoBasilisk <dhnghia92tcv@gmail.com>
2022-04-03 14:47:09 +0700

    Add static file and scss

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const path = require("path");
+const app = express();
+const port = 5000;
+
+// Morgan
+app.use(morgan("tiny"));
+
+//access to static folder/file
+app.use(express.static(path.join(__dirname, "public")));
+
+//Template engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
